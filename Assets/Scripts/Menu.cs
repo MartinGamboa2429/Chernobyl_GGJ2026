5b938c539162b132889c25c43768b61819f3860b
aldana04 <115742447+aldana04@users.noreply.github.com>
2026-02-01 13:03:52 -0300

    Añado escena de lore antes de empezar el juego

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,7 +5,7 @@ public class MenuPrincipal : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene("Juego"); 
+        SceneManager.LoadScene("IntroEscena"); 
     }
 
     public void Creditos()
