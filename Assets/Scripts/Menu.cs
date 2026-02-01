79a157cb1cc29c342aded678b87ab28323854e5f
aldana04 <115742447+aldana04@users.noreply.github.com>
2026-02-01 02:57:05 -0300

    Agrego menu principal y escena de creditos

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Juego"); 
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos"); 
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Salir del juego"); 
+    }
+}
