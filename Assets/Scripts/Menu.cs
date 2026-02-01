742ab7dfa53f94e49ccf19ebf55aefc0a3f41d2e
MartinGamboa2429 <martingamboa2429@gmail.com>
2026-02-01 14:20:28 -0300

    Menu button "Jugar" leads to level

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,7 +5,7 @@ public class MenuPrincipal : MonoBehaviour
 {
     public void Jugar()
     {
-        SceneManager.LoadScene("Juego"); 
+        SceneManager.LoadScene("TestScene"); 
     }
 
     public void Creditos()
